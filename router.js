2f6291e63c80d5d6d720600b1307e38298d392ec
Jamuel <jammmg26@gmail.com>
2020-10-02 17:18:17 +0800

    updated dependencies

--- router.js
+++ router.js
@@ -1,14 +1,14 @@
-const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport');
-const passport = require('passport');
+const Authentication = require("./controllers/authentication");
+const passportService = require("./services/passport");
+const passport = require("passport");
 
-const requireAuth = passport.authenticate('jwt', { session: false });
-const requireSignIn = passport.authenticate('local', { session: false });
+const requireAuth = passport.authenticate("jwt", { session: false });
+const requireSignIn = passport.authenticate("local", { session: false });
 
 module.exports = function (app) {
-  app.get('/', requireAuth, (req, res) => {
-    res.send('hi');
+  app.get("/", requireAuth, (req, res) => {
+    res.send(req.user);
   });
-  app.post('/api/signin', requireSignIn, Authentication.signin);
-  app.post('/api/signup', Authentication.signup);
+  app.post("/api/signin", requireSignIn, Authentication.signin);
+  app.post("/api/signup", Authentication.signup);
 };
