1bdfbf5ce17b8ef24e6f3e57ce781f5a95a23e06
Jamuel Galicia <jammmg26@gmail.com>
2021-10-24 16:26:48 +0800

    fix listeners

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -65,6 +65,7 @@ export const Home = (props) => {
                 charset: 'numeric',
             })
         );
+        socket.removeAllListeners();
     };
 
     const throwFile = (file) => {
--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
-    allowEIO3: true,
+    // allowEIO3: true,
     cors: {
         origin: true,
         credentials: true,
