12da93d170fa9863c85bac80b5682e5f4103db2a
Jamuel Galicia <44177731+jamg26@users.noreply.github.com>
2022-06-12 18:23:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ router(app);
 
 if (['production'].includes(process.env.NODE_ENV)) {
     app.use(express.static(path.join(__dirname, 'build')));
-    app.get("/service-worker.js", (req, res) => {
-      res.sendFile(path.resolve('client', 'build', 'worker.js'));
-    });
+//     app.get("/service-worker.js", (req, res) => {
+//       res.sendFile(path.resolve('client', 'build', 'worker.js'));
+//     });
     app.get('*', (req, res) => {
         res.sendFile(path.resolve('client', 'build', 'index.html'));
     });
